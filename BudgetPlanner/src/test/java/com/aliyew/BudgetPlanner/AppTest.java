bbd483bf6a7d107bfa8a058d86f012bcb60988ac
Aliyew <aliyevulvi0012@gmail.com>
2026-08-04 12:54:31 +0300

    Maven Created

---
+++ BudgetPlanner/src/test/java/com/aliyew/BudgetPlanner/AppTest.java
@@ -0,0 +1,19 @@
+package com.aliyew.BudgetPlanner;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest {
+
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue() {
+        assertTrue(true);
+    }
+}
